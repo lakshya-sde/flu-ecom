94b0ad1c010587c82e32143c33964d7498ac9a2d
Lakshya <lakshygoyal567@gmail.com>
2025-03-09 14:32:24 +0530

    home page curved edge
    v1.2

---
+++ lib/common/widgets/custom_shapes/container/primary_header_conatiner.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/material.dart';
+import 'package:flu_ecom/utils/constants/colors.dart';
+import 'package:flu_ecom/common/widgets/custom_shapes/container/circular_container.dart';
+import 'package:flu_ecom/common/widgets/custom_shapes/curved_edges/curved_edges_widget.dart';
+
+class PrimaryHeaderContainer extends StatelessWidget {
+  const PrimaryHeaderContainer({super.key, required this.child});
+
+  final Widget child;
+  @override
+  Widget build(BuildContext context) {
+    return CurvedEdgeWidget(
+      child: Container(
+        color: TColors.primary,
+        padding: EdgeInsets.only(bottom: 0),
+        child: SizedBox(
+          height: 400,
+          child: Stack(
+            children: [
+              Positioned(
+                top: -150,
+                right: -250,
+                child: TCircularContainer(backgroundColor: TColors.textWhite.withValues(alpha: 0.1)),
+              ),
+              Positioned(
+                top: 100,
+                right: -300,
+                child: TCircularContainer(backgroundColor: TColors.textWhite.withValues(alpha: 0.1)),
+              ),
+              child,
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
--- lib/features/shop/screens/home/home.dart
+++ lib/features/shop/screens/home/home.dart
@@ -1,10 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:flu_ecom/common/widgets/custom_shapes/container/primary_header_conatiner.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(body: SingleChildScrollView(child: Column(children: [PrimaryHeaderContainer(child: Container())])));
   }
 }
